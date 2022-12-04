2b1fcd1b7bd5cc3998c7093b44baa493a959cf0b
Luu Timmy <timmyluu00@gmail.com>
2022-12-04 21:30:07 +0100

    egz

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,18 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template<typename T, typename I>
+void    iter(T *tab, I tab_size, void (*f)(T&)) {
+    for (I i = 0; i < tab_size; i++) {
+        (*f)(tab[i]);
+    }
+}
+
+template<typename T>
+void    display(T &d) {
+    std::cout << d << std::endl;
+} 
+
+#endif
