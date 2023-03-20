a8b0f28522b252f5d4945cf75f38a8facd4306c0
Luu Timmy <timmyluu00@gmail.com>
2023-03-20 12:37:28 +0100

    cpp9

---
+++ cpp09/ex00/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <cstdlib>
+#include <float.h>
+
+size_t dateToInt(std::string date_str)
+{
+    int year, month, day;
+    sscanf(date_str.c_str(), "%d-%d-%d", &year, &month, &day);
+    return year*10000 + month*100 + day;
+}
+
+std::map<size_t, float> readCSV(std::string fileName) {
+    std::map<size_t, float> data;
+    std::ifstream file(fileName.c_str());
+    if (!file.is_open()) {
+        std::cerr << "Error: could not open file." << std::endl;
+        exit(0);
+    }
+    std::string line;
+    getline(file, line);
+    while (getline(file, line)) {
+        std::istringstream iss(line);
+        std::string data_str;
+        std::string value_str;
+        getline(iss, data_str, ',');
+        getline(iss, value_str);
+        if (data_str.empty() || value_str.empty())
+            continue ;
+        data[dateToInt(data_str)] = atof(value_str.c_str());
+    }
+    file.close();
+    return data;
+}
+
+float getExchangeRate(std::map<size_t, float> data, std::string date_str) {
+    size_t date = dateToInt(date_str);
+    
+    std::map<size_t, float>::reverse_iterator rit;
+    for (rit = data.rbegin(); rit != data.rend(); ++rit) {
+        if (rit->first <= date)
+            return rit->second;
+    }
+    return (0);
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: btc <input_file>" << std::endl;
+        return 0;
+    }
+    std::string file = argv[1];
+    std::map<size_t, float> data = readCSV("data.csv");
+
+    std::ifstream input(file.c_str());
+    if (!input.is_open()) {
+        std::cerr << "Error: could not open file." << std::endl;
+        exit(0);
+    }
+
+    std::string line;
+    getline(input, line);
+    while(getline(input, line)) {
+        std::istringstream iss(line);
+        std::string data_str;
+        std::string value_str;
+        getline(iss, data_str, '|');
+        getline(iss, value_str);
+        if (value_str.empty()) {
+            std::cout << "Error: bad input => " << data_str << std::endl; 
+            continue ;
+        }
+        
+        float value;
+        std::stringstream(value_str) >> value;
+        if (value <= 0.0f) {
+            std::cout << "Error: not a positive number." << std::endl;
+            continue;
+        }
+        if (value >= FLT_MAX) {
+            std::cout << "Error: too large a number." << std::endl;
+            continue;
+        }
+        float result = getExchangeRate(data, data_str) * value;
+        //verif error:
+        printf("%s=> %.1f = %.1f\n", data_str.c_str(), value, result);
+    }
+    input.close();
+    return 0;
+}
---
+++ cpp09/ex01/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <stack>
+#include <sstream>
+
+int makeOperation(char token, int op, int op2, std::stack<int> & values) {
+    int result;
+
+    switch(token) {
+        case '+':
+            result = op + op2;
+            break ;
+        case '-':
+            result = op - op2;
+            break ;
+        case '*':
+            result = op * op2;
+            break ; 
+        case '/':
+            if (!op2)
+                return 0;
+            result = op / op2;
+            break ;
+    }
+    values.push(result);
+    return 1;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cout << "Error: Argument" << std::endl;
+        return 0;
+    }
+    std::string token;
+    std::stack<int> values;
+    std::string calcul(argv[1]);
+    std::istringstream ss(calcul);
+
+    while (ss >> token) {
+        if (token == "+" || token == "-" || token == "*" || token == "/") {
+            if (values.size() < 2) {
+                std::cout << "Error: not valid argument" << std::endl;
+                return 0;
+            }
+            int op, op2, result;
+            op2 = values.top();
+            values.pop();
+            op = values.top();
+            values.pop();
+            if (!(result = makeOperation(token[0], op, op2, values))) {
+                std::cout << "Error: division by zero" << std::endl;
+                return 0;
+            }
+        }
+        else {
+            int value;
+            std::istringstream(token) >> value;
+            if (value < 0 || value > 9) {
+                std::cout << "Error: operand " << value << " is out of range" << std::endl;
+                return 0;
+            }
+            values.push(value);
+        }
+    }
+    if (values.size() != 1) {
+        std::cout << "Error: too many operands" << std::endl;
+        return 0;
+    }
+    std::cout << values.top() << std::endl;
+    return 1;
+}
